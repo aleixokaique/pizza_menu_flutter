e135211af2715d6b53ec18bfa04c7611de7b1b2b
aleixokaique <aleixokaique4@gmail.com>
2025-04-08 20:32:00 -0300

    Primeiro widget finalizado

--- lib/menu.dart
+++ lib/menu.dart
@@ -5,8 +5,49 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text("Pizza Menu"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pizza Menu"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            elevation: 2,
+            child: Padding(padding: EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "images/salamino.jpg",
+                    width: 100,
+                    height: 100,
+                  ),
+                  SizedBox(width: 16,),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Texto 1',
+                          style:
+                              TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Texto 2',
+                          style: TextStyle(fontSize: 16, color: Colors.black87),
+                        ),
+                        Text(
+                          'Texto 3',
+                          style: TextStyle(fontSize: 16, color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
