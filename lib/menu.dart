f2435b172fd6ca919d02c86b541fdf725966e40b
aleixokaique <aleixokaique4@gmail.com>
2025-04-08 19:58:27 -0300

    Criação da classe menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -5,6 +5,8 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(title: Text("Pizza Menu"),),
+    );
   }
 }
